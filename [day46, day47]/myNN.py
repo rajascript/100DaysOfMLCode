927fa97c68b410ad9ae48676efad35abec23095b
Raja Raghav <raja_raghav@hotmail.com>
2018-08-21 06:41:52 +0530

    day47
    Former-commit-id: 6e6d5794b73cbcc3d1c4f16f79937f21d9df4314

---
+++ [day46, day47]/myNN.py
@@ -0,0 +1,39 @@
+from numpy import exp,array,random,dot
+class NeuralNetwork():
+	def __init__(self):
+		random.seed(1)
+		
+		self.synaptic_weights = 2 * random.random((3,1)) -1
+		
+	def __sigmoid(self, x):
+		return 1/(1 + exp(-x))
+	def __sigmoid_derivative(self, x):
+		return x * (1-x)
+	def train(self, training_set_inputs, training_set_outputs, number_it):
+		for iter in range(number_it):
+			output = self.predict(training_set_inputs)
+			error = training_set_outputs - output
+			adjustment = dot(training_set_inputs.T, error*self.__sigmoid_derivative(output))
+				
+	def predict(self, inputs):
+		return self.__sigmoid(dot(inputs, self.synaptic_weights))
+	
+if __name__ == '__main__':
+	
+	neural_network = NeuralNetwork()
+	
+	print('Random starting synaptic weights:')
+	print(neural_network.synaptic_weights)
+	
+	inputs = array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
+	outputs = array([[0,1,1,0]]).T
+	
+	neural_network.train(inputs, outputs, 10000)
+
+	print('New synaptic weights after training')
+	print(neural_network.synaptic_weights)
+
+	print('predicting:')
+	
+	print(neural_network.predict(array([1,0,0])))
+	
