473abfe05a211b18320c639b30ed853b91116f45
Raja Raghav <raja_raghav@hotmail.com>
2018-10-13 21:43:40 +0530

    removing
    Former-commit-id: 77338fb351e772bde60130972aa84768a205efa3

---
+++ .git-rewrite/t/day0.py
@@ -0,0 +1,6 @@
+
+import numpy as np;
+
+np.random.rand(5)+1;
+
+print("#100daysofMLcode");
---
+++ .git-rewrite/t/day30/.history/index_20180804121018.js
@@ -0,0 +1,3 @@
+import * as tf from "@tensorflow/tfjs";
+import * as loader from "./loader";
+import * as ui from "./ui";
